cbb0579d5916b724d4f1041281f9a8b381702b92
BobGerman <bgerman1@hotmail.com>
2017-03-20 09:22:24 -0500

    From No Framework template

---
+++ WebPackDemo/src/webparts/webPackDemo/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IWebPackDemoStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'webPackDemoStrings' {
+  const strings: IWebPackDemoStrings;
+  export = strings;
+}
