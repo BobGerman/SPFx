ea9187f82a404697569813f6a6873d843f59f292
BobGerman <bgerman1@hotmail.com>
2017-04-02 01:09:26 -0500

    Remove old WebPack demo

--- WebPackDemo/src/webparts/webPackDemo/IWebPackDemoWebPartProps.ts
+++
@@ -1,3 +0,0 @@
-export interface IWebPackDemoWebPartProps {
-  description: string;
-}
--- WebPackDemo/src/webparts/webPackDemo/tests/WebPackDemo.test.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="mocha" />
-
-import { assert } from 'chai';
-
-describe('WebPackDemoWebPart', () => {
-  it('should do something', () => {
-    assert.ok(true);
-  });
-});
