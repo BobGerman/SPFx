b87f5a7fb6618490e21be33a20e942c313289698
Bob German <bgerman1@hotmail.com>
2018-06-04 19:35:12 -0400

    msGraphClient now works

--- Fetch/src/webparts/fetch/service/SSgraphHttpClient.ts
+++ Fetch/src/webparts/fetch/service/SSgraphHttpClient.ts
@@ -4,30 +4,41 @@ import { IMyInfo } from '../model/IMyInfo';
 import { ISubService } from './ISubService';
 
 import { GraphHttpClient, GraphHttpClientResponse } from '@microsoft/sp-http';
-import { IGraphMeResponse } from './HttpResponses/IGraphMeResponse';
+import { IGraphPeopleResponse } from './HttpResponses/IGraphPeopleResponse';
 
 export class SSgraphHttpClient implements ISubService {
 
     public getInfo(context: IWebPartContext, serviceScope: ServiceScope) {
         return new Promise<IMyInfo | string> ((resolve, reject) => {
-            context.graphHttpClient.get("v1.0/me",
+            context.graphHttpClient.get("v1.0/me/people",
             GraphHttpClient.configurations.v1)
-        .then ((response: GraphHttpClientResponse) => {
-            if (response.ok) {
-                return response.json();
-            } else {
-                throw (`Error ${response.status}: ${response.statusText}`);  
-            }
-        })
-        .then ((o: IGraphMeResponse) => {
-            resolve({
-                "title": "Graph call",
-                "items": [o.displayName]
+            .then ((response: GraphHttpClientResponse) => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    throw (`Error ${response.status}: ${response.statusText}`);  
+                }
+            })
+            .then ((o: IGraphPeopleResponse) => {
+                let x=1;
+                let result: string[] = o.value.slice(0,9).map((v) => {
+                    if (v.givenName || v.surname) {
+                        return v.givenName + " " + v.surname;
+                    } else if (v.scoredEmailAddresses && v.scoredEmailAddresses[0]
+                               && v.scoredEmailAddresses[0].address) {
+                        return v.scoredEmailAddresses[0].address;
+                    } else {
+                        return `User ID ${v.id}`;
+                    }
+                });
+                resolve({
+                    "title": "Graph call",
+                    "items": result
+                });
+            })
+            .catch ((e) => {
+                reject(e);
             });
-        })
-        .catch ((e) => {
-            reject(e);
-        });
         });
     }
 
--- Fetch/src/webparts/fetch/service/SSmsGraphClient.ts
+++ Fetch/src/webparts/fetch/service/SSmsGraphClient.ts
@@ -4,17 +4,44 @@ import { IMyInfo } from '../model/IMyInfo';
 import { ISubService } from './ISubService';
 
 import { MSGraphClient } from '@microsoft/sp-client-preview';
+import { IGraphPeopleResponse } from './HttpResponses/IGraphPeopleResponse';
 
 export class SSmsGraphClient implements ISubService {
 
     public getInfo(context: IWebPartContext, serviceScope: ServiceScope) {
         return new Promise<IMyInfo | string> ((resolve, reject) => {
 
-            resolve ({
-                "title": "Nothing yet",
-                "items": ["Item1","Item2"]
-            });
-            
+            const client: MSGraphClient =
+                serviceScope.consume(MSGraphClient.serviceKey);
+
+            client
+                .api("me/people")
+                .version("v1.0")
+                .get((err, res) => {
+                    
+                    if (err) {
+
+                        reject(err.message);
+
+                    } else {
+
+                        let result: string[] = res.value.slice(0,9).map((v) => {
+                            if (v.givenName || v.surname) {
+                                return v.givenName + " " + v.surname;
+                            } else if (v.scoredEmailAddresses && v.scoredEmailAddresses[0]
+                                       && v.scoredEmailAddresses[0].address) {
+                                return v.scoredEmailAddresses[0].address;
+                            } else {
+                                return `User ID ${v.id}`;
+                            }
+                        });
+                        resolve({
+                            "title": "Graph call",
+                            "items": result
+                        });
+
+                    }
+                });                
         });
     }
 
