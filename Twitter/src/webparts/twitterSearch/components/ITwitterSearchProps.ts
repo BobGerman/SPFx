df7292c7629c6949eed50264df4d5fb09b8073f2
Bob German <bgerman1@hotmail.com>
2019-05-05 14:56:20 -0400

    From yoeman

---
+++ Twitter/src/webparts/twitterSearch/components/ITwitterSearchProps.ts
@@ -0,0 +1,3 @@
+export interface ITwitterSearchProps {
+  description: string;
+}
---
+++ Twitter/src/webparts/twitterSearch/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ITwitterSearchWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'TwitterSearchWebPartStrings' {
+  const strings: ITwitterSearchWebPartStrings;
+  export = strings;
+}
